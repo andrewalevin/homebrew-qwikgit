63a188488f6f363219df2cf5c7e052523c70feaa
Andrew Levin <andrewlevingit@hotmail.com>
2025-02-21 06:04:45 +0300

    2025-02-21 06:04:45 N

--- Formula/qwhawk.rb
+++ Formula/qwhawk.rb
@@ -2,8 +2,8 @@ class Qwhawk < Formula
   desc "Git auto-commit and push with timestamp"
   homepage "https://github.com/andrewalevin/homebrew-qwhawk"
   url "https://raw.githubusercontent.com/andrewalevin/homebrew-qwhawk/main/scripts/qwhawk-git.sh"
-  version "1.0.0"
-  sha256 "e443d9c6c666f983f8155334efb87af2ea7b9f4559f63cd5ae40b665f5af5f29"
+  version "1.0.1"
+  sha256 "e5db8d9609b435899d59e8930ed39be131297bd39b016b4b8d3779d1881602fa"
 
   def install
     bin.install "qwhawk-git.sh" => "qw"
