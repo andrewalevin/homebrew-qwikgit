20e0b916dbbf11e02109b901ec79ac39122fc1b7
Andrew Levin <andrewlevingit@hotmail.com>
2025-02-21 06:10:02 +0300

    2025-02-21 06:10:02 N

--- Formula/qwhawk.rb
+++ Formula/qwhawk.rb
@@ -3,7 +3,7 @@ class Qwhawk < Formula
   homepage "https://github.com/andrewalevin/homebrew-qwhawk"
   url "https://raw.githubusercontent.com/andrewalevin/homebrew-qwhawk/main/scripts/qwhawk-git.sh"
   version "1.0.1"
-  sha256 "e5db8d9609b435899d59e8930ed39be131297bd39b016b4b8d3779d1881602fa"
+  sha256 "15fde252f0c386ed051851fecde0b33de569cb6385bd74f4aff93eddd3e3da92"
 
   def install
     bin.install "qwhawk-git.sh" => "qw"
