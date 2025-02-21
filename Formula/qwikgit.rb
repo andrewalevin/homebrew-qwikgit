ed1ab550903bcb231afbd30e3f8db663aafb216e
Andrew Levin <andrewlevingit@hotmail.com>
2025-02-21 22:46:39 +0300

    2025-02-21 22:46:39

--- Formula/qwikgit.rb
+++ Formula/qwikgit.rb
@@ -2,7 +2,7 @@ class Qwikgit < Formula
   desc "Git auto-commit and push with timestamp"
   homepage "https://github.com/andrewalevin/homebrew-qwikgit"
   url "https://raw.githubusercontent.com/andrewalevin/homebrew-qwikgit/main/scripts/qwikgit.sh"
-  version "1.0.9"  # <- The version is stored ONLY here
+  version "1.1"  # <- The version is stored ONLY here
   sha256 "f57b96391476057c0e544c514a8a1d316e21b18322cf4a5a678a0040cfd9dd56"
 
   def install
